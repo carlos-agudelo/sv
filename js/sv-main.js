fa1af156fa2975d812240959c48f96c22b0c1279
charless-dev <cagudelo20@gmail.com>
2019-07-17 22:32:17 -0500

    avance 17/07

--- js/sv-main.js
+++ js/sv-main.js
@@ -12,3 +12,11 @@ $( ".btn-search" ).click(function(e) {
 	e.preventDefault();
 	$( ".c-search" ).toggleClass('active-search');
 });
+
+$( ".c-icon-menu" ).hover(
+	  function() {
+	    $( ".btn-contact" ).addClass( "hover" );
+	  }, function() {
+	    $( ".btn-contact" ).removeClass( "hover" );
+	  }
+);
