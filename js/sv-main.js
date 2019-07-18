6d0a1a6b947407d52ad219effbd6057f8b8eb651
Ronal henao <ronalhenaodg@gmail.com>
2019-07-17 23:48:13 -0500

    merge

--- js/sv-main.js
+++ js/sv-main.js
@@ -4,4 +4,19 @@ window.onload = function() {
     interval: 2000
   })
 
+
+
 }
+
+$( ".btn-search" ).click(function(e) {
+	e.preventDefault();
+	$( ".c-search" ).toggleClass('active-search');
+});
+
+$( ".c-icon-menu" ).hover(
+	  function() {
+	    $( ".btn-contact" ).addClass( "hover" );
+	  }, function() {
+	    $( ".btn-contact" ).removeClass( "hover" );
+	  }
+);
